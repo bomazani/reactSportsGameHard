908dfa51f331538490bb32bedc04fabbb2a5ff6b
Bob Hunt <bob.hunt.email@gmail.com>
2019-07-08 11:28:21 -0400

    Convert 'Team' component to functional stateless component. Convert functional 'Game' component to stateful component. Getting error... Cannot read property 'score' of undefined.

--- reactApp.js
+++ reactApp.js
@@ -1,19 +1,64 @@
-class Team extends React.Component {
+function Team(props) {
+
+  let shotPercentageDiv
+
+  if (props.stats.shots) {
+    const shotPercentage = Math.round((props.stats.score / props.stats.shots) * 100)
+    shotPercentageDiv = (
+      <div>
+          <strong>Shooting %:</strong> {shotPercentage}
+      </div>
+    )
+  }
+  return (
+    <div className="Team">
+      <h2>{props.name}</h2>
+
+      <div className="identity">
+        <img src={props.logo} alt={props.name} />
+      </div>
+
+      <div>
+        <strong>shots:</strong> {props.stats.shots}
+      </div>
+
+      <div>
+        <strong>score:</strong> {props.stats.score}
+      </div>
+
+      {shotPercentageDiv}
+
+      <button onClick={props.shotHandler}>Shoot!</button>
+    </div>
+  );
+}
+
+
+class Game extends React.Component {
   constructor(props) {
-    super(props);
+    super(props) 
 
-    this.state = {
-      shots: 0,
-      score: 0
-    };
+      this.state = {
+        homeTeamStats: {
+          shots: 0,
+          score: 0
+        },
+        visitingTeamStats: {
+          shots: 0,
+          score: 0
+        },
+      }
 
-    this.shotSound = new Audio('./assets/audio/Bounces.wav');
-    this.scoreSound = new Audio('./assets/audio/Swish+2.wav');
-    this.missSound = new Audio('./assets/audio/Back+Board.wav');
+      this.shotSound = new Audio('./assets/audio/Bounces.wav');
+      this.scoreSound = new Audio('./assets/audio/Swish+2.wav');
+      this.missSound = new Audio('./assets/audio/Back+Board.wav');
+  
   }
 
-  shotHandler = () => {
-    let score = this.state.score;
+  shoot = (team) => {
+    const teamStatsKey = '${team}TeamStats'
+
+    let score = this.state[teamStatsKey].score;
     this.shotSound.play();
 
     if (Math.random() > 0.5) {
@@ -30,71 +75,45 @@ class Team extends React.Component {
   
     setTimeout(() => {
       this.setState((state, props) => ({
-        shots: state.shots + 1,
+        [teamStatsKey] : {
+        shots: state[teamStatsKey].shots + 1,
         score
+        }
       }));
     }, 3000)
-
   };
 
   render() {
-    let shotPercentageDiv
-
-    if (this.state.shots) {
-      const shotPercentage = Math.round((this.state.score / this.state.shots) * 100)
-      shotPercentageDiv = (
-        <div>
-            <strong>Shooting %:</strong> {shotPercentage}
-        </div>
-      )
-    }
     return (
-      <div className="Team">
-        <h2>{this.props.name}</h2>
+      <div className="Game">
+        <h1>Welcome to {this.props.venue}</h1>
+        <div className="stats">
+          <Team
+            name={this.props.homeTeam.name}
+            logo={this.props.homeTeam.logoSrc}
+            stats={this.state.homeTeamStats}
+            shotHandler={() => this.shoot('home')}
+          />
 
-        <div className="identity">
-          <img src={this.props.logo} alt={this.props.name} />
+          <div className="versus">
+            <h1>VS</h1>
+            <div>
+              
+            </div>
+          </div>
+
+          <Team
+            name={this.props.visitingTeam.name}
+            logo={this.props.visitingTeam.logoSrc}
+            stats={this.state.visitingTeamStats}
+            shotHandler={() => this.shoot('visiting')}
+          />
         </div>
-
-        <div>
-          <strong>shots:</strong> {this.state.shots}
-        </div>
-
-        <div>
-          <strong>score:</strong> {this.state.score}
-        </div>
-
-        {shotPercentageDiv}
-
-        <button onClick={this.shotHandler}>Shoot!</button>
       </div>
-    );
+    )
   }
 }
-
-function Game(props) {
-  return (
-    <div className="Game">
-      <h1>Welcome to {props.venue}</h1>
-      <div className="stats">
-        <Team
-          name={props.homeTeam.name}
-          logo={props.homeTeam.logoSrc}
-        />
-
-        <div className="versus">
-          <h1>VS</h1>
-        </div>
-
-        <Team
-          name={props.visitingTeam.name}
-          logo={props.visitingTeam.logoSrc}
-        />
-      </div>
-    </div>
-  )
-}
-
+  
 // Default App component that all other compents are rendered through
 function App(props) {
   const ducks = {
